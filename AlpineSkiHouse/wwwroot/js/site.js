6f44079200d504cd43e62c752ce2adb5cac6e93f
Yratilos <2197837920@qq.com>
2023-05-25 13:22:59 +0800

    添加js代码导出文件

--- AlpineSkiHouse/wwwroot/js/site.js
+++ AlpineSkiHouse/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var Tool = {
+    /**
+     * 导出Excel
+     * @param {string} methd 请求类型
+     * @param {string} url 请求地址
+     * @param {string} fileName 文件名
+     */
+    exportExcel: function (methd, url, fileName) {
+        var oReq = new XMLHttpRequest()
+        oReq.responseType = "blob";
+        oReq.open(methd, url)
+        oReq.send();
+        // 请求已完成，且响应已就绪
+        oReq.onload = function (oEvent) {
+            if (oReq.status >= 200 && oReq.status < 300) {
+                var blob = oReq.response;
+                var objectUrl = URL.createObjectURL(blob);
+                if (window.navigator.msSaveBlob) {
+                    window.navigator.msSaveBlob(blob, fileName)
+                } else {
+                    var a = document.createElement('a');
+                    document.body.appendChild(a);
+                    a.style = 'display:none'
+                    a.href = objectUrl
+                    a.download = fileName
+                    a.click();
+                    URL.revokeObjectURL(objectUrl);
+                }
+            }
+        };
+    }
+}
