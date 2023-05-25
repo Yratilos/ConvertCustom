b62461528bdb02bf6eac68273da8fcc96190f7dc
Yratilos <2197837920@qq.com>
2023-05-25 14:29:02 +0800

    js文件导出

--- AlpineSkiHouse/wwwroot/js/site.js
+++ AlpineSkiHouse/wwwroot/js/site.js
@@ -4,14 +4,14 @@
 // Write your JavaScript code.
 var Tool = {
     /**
-     * 导出Excel
+     * 导出Excel（Blob）
      * @param {string} methd 请求类型
-     * @param {string} url 请求地址
+     * @param {string} url 请求地址（注意参数需要编码后传递）
      * @param {string} fileName 文件名
      */
     exportExcel: function (methd, url, fileName) {
         var oReq = new XMLHttpRequest()
-        oReq.responseType = "blob";
+        oReq.responseType = 'blob';
         oReq.open(methd, url)
         oReq.send();
         // 请求已完成，且响应已就绪
@@ -32,5 +32,31 @@ var Tool = {
                 }
             }
         };
+    },
+    /**
+     * 导出Excel（Form）
+     * @param {string} methd 请求类型
+     * @param {string} url 请求地址需要返回contentType为application/vnd.ms-excel类型的数据，自带文件名
+     * @param {Array} childs [{Key:test,Value:test},{Key:test,Value:test}]
+     */
+    exportExcel: function (method, url, childs = []) {
+        function createInput(dic) {
+            var input = document.createElement('input')
+            input.name = dic['Key']
+            input.value = dic['Value']
+            return input
+        }
+        var form = document.createElement('form')
+        form.action = url
+        form.enctype = 'multipart/form-data'
+        form.method = method
+        if (childs.length > 0) {
+            for (var i = 0; i < childs.length; i++) {
+                form.appendChild(createInput(childs[i]))
+            }
+        }
+        document.body.appendChild(form)
+        form.submit();
+        form.remove();
     }
 }
