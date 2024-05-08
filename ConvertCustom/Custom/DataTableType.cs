15ebfddebccaa55bc96f44a7bc420fa34beee5d3
Yratilos <2197837920@qq.com>
2024-05-08 17:51:57 +0800

    新增方法:DataTable相关方法

--- ConvertCustom/Custom/DataTableType.cs
+++ ConvertCustom/Custom/DataTableType.cs
@@ -11,6 +11,71 @@ namespace ConvertCustom.Custom
     public static class DataTableType
     {
         /// <summary>
+        /// 合并表(不去重)
+        /// </summary>
+        public static DataTable Merge(DataTable dt1, DataTable dt2)
+        {
+            // 创建一个新的DataTable用于存储合并后的数据
+            DataTable mergedTable = new DataTable();
+
+            // 将dt1的列添加到mergedTable
+            foreach (DataColumn column in dt1.Columns)
+            {
+                mergedTable.Columns.Add(column.ColumnName, column.DataType);
+            }
+
+            // 使用流式处理将dt1的数据添加到mergedTable
+            foreach (DataRow row in dt1.Rows)
+            {
+                mergedTable.Rows.Add(row.ItemArray);
+            }
+
+            // 使用流式处理将dt2的数据添加到mergedTable
+            foreach (DataRow row in dt2.Rows)
+            {
+                mergedTable.Rows.Add(row.ItemArray);
+            }
+
+            return mergedTable;
+        }
+        
+        /// <summary>
+        /// 合并表去重
+        /// </summary>
+        /// <param name="dt1">主表</param>
+        /// <param name="dt2">副表</param>
+        /// <param name="Id">去重字段</param>
+        public static DataTable MergeAndRemoveDuplicates(DataTable dt1, DataTable dt2,string Id) 
+        { 
+            DataTable dataTable = new DataTable(); 
+            foreach (DataColumn column in dt1.Columns) { 
+                dataTable.Columns.Add(column.ColumnName, column.DataType); 
+                }
+            foreach (DataRow row in dt1.Rows)
+            {
+                dataTable.Rows.Add(row.ItemArray);
+            }
+
+            foreach (DataRow row2 in dt2.Rows)
+            {
+                bool duplicate = false;
+                foreach (DataRow existingRow in dataTable.Rows)
+                {
+                    if (row2[Id].Equals(existingRow[Id]))
+                    {
+                        duplicate = true;
+                        break;
+                    }
+                }
+                if (!duplicate)
+                {
+                    dataTable.Rows.Add(row2.ItemArray);
+                }
+            }
+
+            return dataTable;
+        }
+        /// <summary>
         /// 泛型K不能为空
         /// </summary>
         public static DataTable Parse<K, V>(List<Dictionary<K, V>> listDic)
